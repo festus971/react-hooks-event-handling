94a9a84c2b0d894fd725577e7ebeef720c197a22
+ <+>
2022-07-13 10:45:05 +0300

    one

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,6 +10,7 @@ function Login() {
     <form onSubmit={handleSubmit}>
       <input type="text" name="username" placeholder="Enter username..." />
       <input type="password" name="password" placeholder="Enter password..." />
+      <input type="true" name="one one" placeholder="enter one..."/>
       <button>Login</button>
     </form>
   );
